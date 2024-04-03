06401aa48f8a8ffeddb5e6b8668e23f4f756afbe
Majdi Bnenboubaker <majdiboubaker82@gmail.com>
2024-04-03 23:49:25 +0100

    Delete Order-Stock MicroServices directory

--- Order-Stock MicroServices/OrderService/Microservices-Project-Kafka/src/main/java/com/example/OrderProduct/Repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.OrderProduct.Repository;
-
-import com.example.OrderProduct.Model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository <Order, String> {
-}
